564e090f28bc7d4ed1a5b6f6878547c079166b07
TA <tomasz.abratowski@gmail.com>
2024-05-25 17:31:55 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/tabratowski/goTest/timeout"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(100)
+	ctx := context.Background()
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			defer wg.Done()
+			err := timeout.Timeout(ctx, time.Duration(rand.Intn(100)+50), func(ctx context.Context) error {
+				for i := 0; i < 2; i++ {
+					if ctx.Err() != nil {
+						fmt.Println("Cancelled func", ctx.Err())
+						return fmt.Errorf("cancelled from fn")
+					}
+					time.Sleep(time.Duration(rand.Intn(60) + 25))
+				}
+				return nil
+			})
+			fmt.Println(err)
+		}(i)
+	}
+	wg.Wait()
+}
---
+++ timeout/timeout.go
@@ -0,0 +1,34 @@
+package timeout
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Timeout(ctx context.Context, d time.Duration, fn func(ctx context.Context) error) error {
+	done := make(chan struct{})
+	defer close(done)
+	var err error
+	mu := &sync.Mutex{}
+	ctx, cancel := context.WithTimeout(ctx, d)
+	defer cancel()
+	go func() {
+		mu.Lock()
+		err = fn(ctx)
+		defer mu.Unlock()
+		if ctx.Err() != nil {
+			return
+		}
+		done <- struct{}{}
+	}()
+	select {
+	case <-ctx.Done():
+		return errors.New("context has been cancelled")
+	case <-done:
+		fmt.Println("done")
+		return err
+	}
+}
