e6ba80facb103c37ee13df16e7c1984357e138e4
TA <tomasz.abratowski@gmail.com>
2024-11-11 18:06:16 +0100

    Safe channel introduced to properly handle closing generator

--- generatorFn/generator_fn_test.go
+++ generatorFn/generator_fn_test.go
@@ -6,15 +6,16 @@ import (
 	"testing"
 )
 
-func genFn(yield func(res Result[int])) {
+func genFn(yield func(res Result[int]) bool) {
 	for i := range 10 {
-		yield(Result[int]{Value: i})
-		fmt.Println(i)
+		if !yield(Result[int]{Value: i}) {
+			return
+		}
 	}
 	fmt.Println("done")
 }
 
-func genFn2(yield func(res Result[int])) {
+func genFn2(yield func(res Result[int]) bool) {
 	yield(Result[int]{Value: 10})
 	yield(Result[int]{Value: 20})
 	yield(Result[int]{Value: 30})
@@ -23,7 +24,7 @@ func genFn2(yield func(res Result[int])) {
 func TestNewGeneratorFn(t *testing.T) {
 	for _, test := range []struct {
 		name        string
-		genFn       func(yield func(res Result[int]))
+		genFn       func(yield func(res Result[int]) bool)
 		expectedLen int
 	}{
 		{
@@ -43,6 +44,7 @@ func TestNewGeneratorFn(t *testing.T) {
 			var allResults []int
 			// Act
 			gen.Start()
+			defer gen.Close()
 			for {
 				if res, ok := gen.Next(); ok {
 					allResults = append(allResults, res.Value)
@@ -54,6 +56,39 @@ func TestNewGeneratorFn(t *testing.T) {
 
 			// Assert
 			assert.Len(t, allResults, test.expectedLen)
+		})
+	}
+}
+
+func TestGeneratorFn_Next(t *testing.T) {
+	for _, test := range []struct {
+		name           string
+		nextCalls      int
+		genFn          func(yield func(res Result[int]) bool)
+		expectedResult Result[int]
+	}{
+		{
+			name:           "Next - should stop on second yield",
+			nextCalls:      2,
+			genFn:          genFn,
+			expectedResult: Result[int]{Value: 1},
+		},
+		{
+			name:           "Next - should stop on seventh yield",
+			nextCalls:      7,
+			genFn:          genFn,
+			expectedResult: Result[int]{Value: 6},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			gen := NewGeneratorFn(test.genFn)
+			gen.Start()
+			defer gen.Close()
+			var result Result[int]
+			for i := 0; i < test.nextCalls; i++ {
+				result, _ = gen.Next()
+			}
+			assert.Equal(t, test.expectedResult, result)
 		})
 	}
 }
